ed34478f390abf227f0f349ef69033b5b265ec7e
ajie5419 <ajie5419@gmail.com>
2026-02-14 16:24:41 +0800

    feat(qms): add vehicle commissioning view module

---
+++ apps/web-antd/src/views/qms/vehicle-commissioning/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { Page } from '@vben/common-ui';
+import { useI18n } from '@vben/locales';
+
+import { Empty } from 'ant-design-vue';
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <Page content-class="p-4">
+    <div
+      class="flex min-h-[360px] items-center justify-center rounded-lg border border-dashed border-gray-200 bg-white"
+    >
+      <Empty :description="t('common.comingSoon')" />
+    </div>
+  </Page>
+</template>
