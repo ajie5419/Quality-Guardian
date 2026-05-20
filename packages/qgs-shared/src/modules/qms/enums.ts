0080e11df3620c55c7bedc92505efa58a05b9857
ajie5419 <ajie5419@gmail.com>
2026-05-20 17:00:10 +0800

    style(project): keep qgs-shared enum re-export format stable

--- packages/qgs-shared/src/modules/qms/enums.ts
+++ packages/qgs-shared/src/modules/qms/enums.ts
@@ -1,11 +1,3 @@
-import {
-  InspectionIssueStatusEnum,
-  ProjectStatusEnum,
-  QualityLossStatusEnum,
-  SupplierStatusEnum,
-  WorkOrderStatusEnum,
-} from '@qgs/enums';
-
 /**
  * QMS Business Logic Enums
  */
@@ -13,24 +5,27 @@ import {
 /**
  * QMS Supplier Status
  */
-export { SupplierStatusEnum };
 
 /**
  * QMS Work Order Status
  */
-export { WorkOrderStatusEnum };
 
 /**
  * QMS Inspection Issue Status
  */
-export { InspectionIssueStatusEnum };
 
 /**
  * QMS Quality Loss Status
  */
-export { QualityLossStatusEnum };
 
 /**
  * QMS Project Status (Planning)
  */
-export { ProjectStatusEnum };
+
+export {
+  InspectionIssueStatusEnum,
+  ProjectStatusEnum,
+  QualityLossStatusEnum,
+  SupplierStatusEnum,
+  WorkOrderStatusEnum,
+} from '@qgs/enums';
