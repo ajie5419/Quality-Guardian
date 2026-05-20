6f381a8c9d62d988ed8eb3f0f4a1293244f584c4
ajie5419 <ajie5419@gmail.com>
2026-05-20 13:35:06 +0800

    refactor(project): extract work order attachment rules

--- packages/qgs-domain/src/index.ts
+++ packages/qgs-domain/src/index.ts
@@ -29,4 +29,5 @@ export * from './modules/qms/task-dispatch';
 export * from './modules/qms/welder-score';
 export * from './modules/qms/work-order';
 export * from './modules/qms/work-order-requirement-summary';
+export * from './modules/qms/work-order-requirement-attachments';
 export * from './modules/qms/work-order-status';
